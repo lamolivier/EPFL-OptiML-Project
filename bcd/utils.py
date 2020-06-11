0fb8f4cd1e79642b3d4013939a460d2c73fefb4a
rayanelaraki(iJ) <rayane.laraki@epfl.ch>
2020-06-11 14:26:59 +0100

    dataloader

--- bcd/utils.py
+++ bcd/utils.py
@@ -7,10 +7,10 @@ from bcd.ModelBCD import ModelBCD
 import time
 from .metrics import *
 from dfw.ModelDFW import ModelDFW
-from torch.utils.data import DataLoader
 
 
-def full_train_test(optimizer, N_train, N_test, n_iter, n_epochs, batch_size = 0, d1 = 200, d2 = 200, d3 = 200, gamma = 1, alpha = 1, rho = 1, verbose = False):
+
+def full_train_test(optimizer, N_train, N_test, n_iter, n_epochs, batch_size = 1, d1 = 200, d2 = 200, d3 = 200, gamma = 1, alpha = 1, rho = 1, verbose = False):
     
     # Init metrics arrays
     train_loss_matrix = []
@@ -20,7 +20,9 @@ def full_train_test(optimizer, N_train, N_test, n_iter, n_epochs, batch_size = 0
     accuracy_train_array = []
     time_array = []
     
-    mnist_trainset, mnist_testset = generate_pair_sets()
+    train_set, test_set = get_sets()
+    
+    train_data, test_data = generate_pair_sets(train_set, test_set, N_train, N_test, batch_size)
     
     d0 =  1*28*28
     
@@ -30,12 +32,10 @@ def full_train_test(optimizer, N_train, N_test, n_iter, n_epochs, batch_size = 0
         start_time = time.time()
         
         if (optimizer == "BCD"):
-            train_input, train_target, y_train_1hot, test_input, test_target, y_test_1hot = preprocess_data(mnist_trainset, mnist_testset, N_train, N_test)
+            train_input, train_target, y_train_1hot, test_input, test_target, y_test_1hot = preprocess_data(train_data, test_data, N_train, N_test)
             model = ModelBCD(d0, d1, d2, d3, 10, gamma, alpha, rho)
             tr_losses, te_losses, tr_acc, te_acc = model.train(n_epochs, train_input,train_target,y_train_1hot, test_input, test_target, y_test_1hot, verbose = verbose)
         else:
-            train_data = DataLoader(mnist_trainset,batch_size=batch_size)
-            test_data = DataLoader(mnist_testset,batch_size=batch_size)
             model = ModelDFW(d0, d1, d2, d3, 10)
             tr_losses, te_losses, tr_acc, te_acc = model.train(train_data, test_data, n_epochs, verbose = verbose)
 
@@ -46,15 +46,13 @@ def full_train_test(optimizer, N_train, N_test, n_iter, n_epochs, batch_size = 0
         
         if (optimizer == "BCD"):
         # Compute test accuracy
-            acc_test = accuracy(model, N_test, mnist_trainset, mnist_testset)
-            acc_train = accuracy(model, N_train, mnist_trainset, mnist_testset)
+            acc_test = accuracy(model, N_test, train_set, test_set)
         else: 
-            test_data = DataLoader(mnist_testset,batch_size=batch_size, shuffle = True)
+            _, test_data = generate_pair_sets(train_set, test_set, N_train, N_test, batch_size)
             acc_test = model.test(test_data, batch_size)
-            acc_train = model.test(train_data, batch_size)
         
         accuracy_test_array.append(acc_test)
-        accuracy_train_array.append(acc_train)
+        accuracy_train_array.append(tr_acc[n_epochs-1])
         
         time_array.append(end_time - start_time)
     
